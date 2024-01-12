3926d48ce3c6667e8178163d351bb9edccae2b59
CG_Lab <gchakradhar72@gmail.com>
2024-01-12 15:15:08 -0500

    Updates page content

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const Home: React.FC = () => {
     return (
-        <div>
-            Home Page
-        </div>
+        <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
+            <Typography variant="h1">Home Page</Typography>
+        </Box>
     );
 };
 
--- web/src/pages/Temp2.tsx
+++ web/src/pages/Temp2.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const Temp2: React.FC = () => {
     return (
-        <div>
-            Placeholder Page 2
-        </div>
+        <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
+            <Typography variant="h1">Placeholder Page 2</Typography>
+        </Box>
     );
 };
 
