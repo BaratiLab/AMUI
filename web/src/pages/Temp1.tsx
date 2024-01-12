044c57ddd0b65fd793dfe50f1b3f88056395cd9f
CG_Lab <gchakradhar72@gmail.com>
2024-01-12 14:34:39 -0500

    Adds new pages: Home, Temp1, Temp2

---
+++ web/src/pages/Temp1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Temp1: React.FC = () => {
+    return (
+        <div>
+            Placeholder Page 1
+        </div>
+    );
+};
+
+export default Temp1;
---
+++ web/src/pages/Temp2.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Temp2: React.FC = () => {
+    return (
+        <div>
+            Placeholder Page 2
+        </div>
+    );
+};
+
+export default Temp2;
