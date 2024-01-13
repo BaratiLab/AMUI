c7f463210c3c0f95c0be502e74c31cc23a8d40a6
CG_Lab <gchakradhar72@gmail.com>
2024-01-12 21:02:12 -0500

    Refactors component imports

--- web/src/pages/Temp1.tsx
+++ web/src/pages/Temp1.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
 
-const Temp1: React.FC = () => {
+const Temp1: FC = () => {
     return (
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
             <Typography variant="h1">Placeholder Page 1</Typography>
--- web/src/pages/Temp2.tsx
+++ web/src/pages/Temp2.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
 
-const Temp2: React.FC = () => {
+const Temp2: FC = () => {
     return (
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
             <Typography variant="h1">Placeholder Page 2</Typography>
