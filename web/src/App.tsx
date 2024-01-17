58a6c16571c8377cc22c4ccf85b135857221365b
Achuth Chandrasekhar <achuthchandrasekhar@achuths-air.wifi.local.cmu.edu>
2024-01-17 17:58:48 -0500

    inline style corrected in navbar code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,18 +4,27 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Temp1 from './pages/Temp1';
 import Temp2 from './pages/Temp2';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { theme } from './theme';
 
 
 const App: FC = () => {
     return (
-        <div>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/temp1" element={<Temp1 />} />
-                <Route path="/temp2" element={<Temp2 />} />
-            </Routes>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container>
+                <div>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/temp1" element={<Temp1 />} />
+                        <Route path="/temp2" element={<Temp2 />} />
+                    </Routes>
+                </div>
+            </Container>
+        </ThemeProvider>
     );
 };
 
