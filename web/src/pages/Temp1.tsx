f762b4d0f7adf3e7e31a3f4a9dd2db0a6be2508a
CG_Lab <gchakradhar72@gmail.com>
2024-01-18 17:22:20 -0500

    Adds STLViewer component and integrate it into Temp1 page

--- web/src/pages/Temp1.tsx
+++ web/src/pages/Temp1.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
+import STLViewer from '../components/STLViewer';
 
 const Temp1: FC = () => {
     return (
-        <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
-            <Typography variant="h1">Placeholder Page 1</Typography>
+        <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column'>
+            <Typography variant='h3'>STL Viewer</Typography>
+            <STLViewer height={600} width={600} />
         </Box>
     );
 };
