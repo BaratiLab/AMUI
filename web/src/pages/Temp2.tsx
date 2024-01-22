baae8493be20a4429b67c42568e8eae280fbb818
Achuth Chandrasekhar <achuthchandrasekhar@achuths-air.wifi.local.cmu.edu>
2024-01-22 16:16:01 -0500

    created a table component in the temp 2 page

--- web/src/pages/Temp2.tsx
+++ web/src/pages/Temp2.tsx
@@ -1,11 +1,24 @@
 import { FC } from 'react';
 import { Box, Typography } from '@mui/material';
+import DenseTable from '../components/table';
+import { theme } from '../theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 const Temp2: FC = () => {
     return (
+        <ThemeProvider theme={theme}>
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
-            <Typography variant="h1">Placeholder Page 2</Typography>
+            
+              {/*<Typography variant="h1">Placeholder Page 2</Typography> */}
+
+             <div>
+             <DenseTable />
+             </div>  
         </Box>
+        </ThemeProvider>
+        
+        
+    
     );
 };
 
