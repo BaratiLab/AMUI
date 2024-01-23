3039b101cfdfff5ccc9b4e81014c00dde4281d95
Achuth Chandrasekhar <achuthchandrasekhar@achuths-air.mynetworksettings.com>
2024-01-22 22:48:53 -0500

    removed redundant lines of code in temp 2 file

--- web/src/pages/Temp2.tsx
+++ web/src/pages/Temp2.tsx
@@ -1,21 +1,20 @@
 import { FC } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box} from '@mui/material';
 import DenseTable from '../components/table';
-import { theme } from '../theme';
-import { ThemeProvider } from '@mui/material/styles';
+
 
 const Temp2: FC = () => {
     return (
-        <ThemeProvider theme={theme}>
+        
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
             
-              {/*<Typography variant="h1">Placeholder Page 2</Typography> */}
+              
 
              <div>
              <DenseTable />
              </div>  
         </Box>
-        </ThemeProvider>
+       
         
         
     
