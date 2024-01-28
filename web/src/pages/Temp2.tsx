f124bea58b9e4402517a4a840a85f7e90429997c
CG_Lab <gchakradhar72@gmail.com>
2024-01-27 19:08:42 -0500

    Adds data fetching and parsing from csv file

--- web/src/pages/Temp2.tsx
+++ web/src/pages/Temp2.tsx
@@ -1,18 +1,24 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Box } from '@mui/material';
+import * as Papa from 'papaparse';
 import DenseTable from '../components/table';
-
-
-const colNames = ['Dessert (100g serving)', 'Calories', 'Fat (g)', 'Carbs (g)', 'Protein (g)'];
-const rows = [
-    ['Frozen yoghurt', '159', '6', '24', '4'],
-    ['Ice cream sandwich', '237', '9', '37', '4.3'],
-    ['Eclair', '262', '16', '24', '6'],
-    ['Cupcake', '305', '3.7', '67', '4.3'],
-    ['Gingerbread', '356', '16', '49', '3.9']
-];
+// @ts-ignore
+import meltpoolGeo from "../meltpoolgeometry.csv";
 
 const Temp2: FC = () => {
+    const [ colNames, setColNames ] = useState<Array<string>>([]);
+    const [ rows, setRows ] = useState<Array<Array<string>>>([]);
+
+    useEffect(() => {
+        fetch( meltpoolGeo )
+            .then( response => response.text() )
+            .then( responseText => {
+                const data = Papa.parse(responseText);
+                setColNames( data.data[0] as string[] );
+                setRows( data.data.slice(1, 50) as string[][] );    // TODO: paginate to display all rows
+            });
+    }, []);
+
     return (
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
             <div>
