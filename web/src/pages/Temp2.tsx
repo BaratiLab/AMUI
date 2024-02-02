9caa0737fc1e00f80ae2a97ddf2063570cfc6753
CG_Lab <gchakradhar72@gmail.com>
2024-02-02 14:19:51 -0500

    Moves common logic into separate component

--- web/src/pages/Temp2.tsx
+++ web/src/pages/Temp2.tsx
@@ -1,28 +1,12 @@
 import { FC, useEffect, useState } from 'react';
-import { Box } from '@mui/material';
-import DenseTable from '../components/Table';
+import Viz from '../components/DataViz';
 // @ts-ignore
 import meltpoolGeo from "../meltpoolgeometry.csv";
 
 const Temp2: FC = () => {
-    const [ colNames, setColNames ] = useState<Array<string>>([]);
-    const [ rows, setRows ] = useState<Array<Array<string>>>([]);
-
-    useEffect(() => {
-        setColNames(Object.keys(meltpoolGeo[0]));
-    }, [meltpoolGeo]);
-
-    useEffect(() => {
-        setRows(meltpoolGeo.map((row: any) => colNames.map((colName: string) => row[colName])));
-    }, [colNames]);
-
     return (
-        <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
-            <div>
-                <DenseTable colNames={colNames} rows={rows} />
-            </div>
-        </Box>
-    );
+        <Viz meltpoolData={meltpoolGeo}/>
+    )
 };
 
 export default Temp2;
