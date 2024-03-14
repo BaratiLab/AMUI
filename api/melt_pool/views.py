e87ee20e1385ed1d2da6d923a5b68990a37f7c1b
CG_Lab <gchakradhar72@gmail.com>
2024-03-13 23:27:51 -0400

    Adds inference API endpoint for classification model

--- api/melt_pool/views.py
+++ api/melt_pool/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import pickle
+import numpy as np
 
 from melt_pool.models import Record
 from melt_pool.serializers import RecordSerializer
@@ -47,3 +49,25 @@ class ProcessParametersDict(APIView):
             unique_values[field_name] = sorted([x for x in values if x is not None])
 
         return Response(unique_values)
+
+class Inference(APIView):
+    """
+    Load classfication model and run inference
+    """
+
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        with open('./melt_pool/rf.pt', 'rb') as f:
+            model = pickle.load(f)
+        with open('./melt_pool/material_mapping.pkl', 'rb') as f:
+            material_mapping = pickle.load(f)
+
+        material = request.query_params.get('mat')
+        power = float(request.query_params.get('p'))
+        velocity = float(request.query_params.get('v'))
+
+        features = np.array([power, velocity, *material_mapping[material]]).reshape(1, -1)
+        prediction = model.predict(features)
+
+        return Response({'prediction': prediction[0]})
